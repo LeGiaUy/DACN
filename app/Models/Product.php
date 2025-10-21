cd9e909e5011f7a29c96c19ee8d3e3a9d18e302c
Le Gia Uy <leuy091104@gmail.com>
2025-10-21 20:28:08 +0700

    Migrations & Models

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'img_url', 'price', 'description', 'category_id', 'brand_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+}
